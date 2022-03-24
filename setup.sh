a5f30c9ae207e80b32bc06e93f0ff391be831ed9
Jebaitedneko <Jebaitedneko@gmail.com>
2022-03-24 13:37:44 +0530

    cleanup

--- setup.sh
+++ setup.sh
@@ -22,51 +22,14 @@ rem() {
 get() {
     curl -LSs  "https://codeload.github.com/$1/zip/$2" -o "$3".zip
     unzip "$3".zip -d. && rm "$3".zip && mv -v "${1##*/}-$2" "/usr/${3}"
-    find "/usr/${3}" -exec chmod +x {} \; && rm -rfv "/usr/${3}/.git"
-    rem "$3"
-}
-
-get_llvm() {
-    SRC=https://raw.githubusercontent.com/mvaisakh
-    [[ $1 =~ '32' ]] && VSN='' || VSN=64
-    [[ $3 != '' ]] && SHA="$3" || SHA="gcc-master"
-    curl -LSs "$SRC/gcc-arm${VSN}/${SHA}/bin/lld" -o "$2/lld" && chmod +x "$2/lld"
-    curl -LSs "$SRC/gcc-arm${VSN}/${SHA}/bin/llvm-ar" -o "$2/llvm-ar" && chmod +x "$2/llvm-ar"
-    curl -LSs "$SRC/gcc-arm${VSN}/${SHA}/bin/llvm-as" -o "$2/llvm-as" && chmod +x "$2/llvm-as"
-    curl -LSs "$SRC/gcc-arm${VSN}/${SHA}/bin/llvm-nm" -o "$2/llvm-nm" && chmod +x "$2/llvm-nm"
-    curl -LSs "$SRC/gcc-arm${VSN}/${SHA}/bin/llvm-objdump" -o "$2/llvm-objdump" && chmod +x "$2/llvm-objdump"
-    curl -LSs "$SRC/gcc-arm${VSN}/${SHA}/bin/llvm-objcopy" -o "$2/llvm-objcopy" && chmod +x "$2/llvm-objcopy"
-    curl -LSs "$SRC/gcc-arm${VSN}/${SHA}/bin/llvm-readobj" -o "$2/llvm-readobj" && chmod +x "$2/llvm-readobj"
-    ln -sv "$(realpath "$2/lld")" "$(realpath "$2")/ld.lld"
-    ln -sv "$(realpath "$2/llvm-readobj")" "$(realpath "$2")/llvm-readelf"
-    ln -sv "$(realpath "$2/llvm-objcopy")" "$(realpath "$2")/llvm-strip"
-}
-
-get_binutils() {
-    SRC=https://raw.githubusercontent.com/mvaisakh
-    if [[ $1 =~ '32' ]];then  VSN='' && PFX='arm-eabi'; else VSN=64 && PFX='aarch64-elf'; fi
-    [[ $3 != '' ]] && SHA="$3" || SHA="gcc-master"
-    [[ $4 != '' ]] && PFX_OVER="$4" || PFX_OVER="$PFX-"
-    curl -LSs "$SRC/gcc-arm${VSN}/${SHA}/bin/${PFX}-ar" -o "$2/${PFX_OVER}ar" && chmod +x "$2/${PFX_OVER}ar"
-    curl -LSs "$SRC/gcc-arm${VSN}/${SHA}/bin/${PFX}-as" -o "$2/${PFX_OVER}as" && chmod +x "$2/${PFX_OVER}as"
-    curl -LSs "$SRC/gcc-arm${VSN}/${SHA}/bin/${PFX}-nm" -o "$2/${PFX_OVER}nm" && chmod +x "$2/${PFX_OVER}ld"
-    curl -LSs "$SRC/gcc-arm${VSN}/${SHA}/bin/${PFX}-nm" -o "$2/${PFX_OVER}nm" && chmod +x "$2/${PFX_OVER}nm"
-    curl -LSs "$SRC/gcc-arm${VSN}/${SHA}/bin/${PFX}-readelf" -o "$2/${PFX_OVER}elfedit" && chmod +x "$2/${PFX_OVER}elfedit"
-    curl -LSs "$SRC/gcc-arm${VSN}/${SHA}/bin/${PFX}-objdump" -o "$2/${PFX_OVER}objdump" && chmod +x "$2/${PFX_OVER}objdump"
-    curl -LSs "$SRC/gcc-arm${VSN}/${SHA}/bin/${PFX}-objcopy" -o "$2/${PFX_OVER}objcopy" && chmod +x "$2/${PFX_OVER}objcopy"
-    curl -LSs "$SRC/gcc-arm${VSN}/${SHA}/bin/${PFX}-readelf" -o "$2/${PFX_OVER}readelf" && chmod +x "$2/${PFX_OVER}readelf"
-    curl -LSs "$SRC/gcc-arm${VSN}/${SHA}/bin/${PFX}-strip" -o "$2/${PFX_OVER}strip" && chmod +x "$2/${PFX_OVER}strip"
+    find "/usr/${3}" -exec chmod +x {} \; && rem "$3"
 }
 
 get XSans02/Weeb-Clang main clang
 get mvaisakh/gcc-arm64 gcc-master gcc64
 get mvaisakh/gcc-arm gcc-master gcc32
 
-cd /usr/clang && rep 'Weeb' 'Zero' && rep 'github.com/llvm/llvm-project' 'youtu.be/watch?v=dQw4w9WgXcQ' && cd /
-
-# get_llvm 64 /usr/clang/bin 6595077c606eb70344f85259630db7964f6fc5fc # 20220208
-# get_binutils 64 /usr/gcc64/bin 6595077c606eb70344f85259630db7964f6fc5fc # 20220208
-# get_binutils 32 /usr/gcc32/bin cf08471c4ff7285db1532076c8bce1e13174ff7d # 20220208
+cd /usr/clang && rep 'Weeb' 'Zer0' && rep 'github.com/llvm/llvm-project' 'youtu.be/watch?v=dQw4w9WgXcQ' && cd /
 
 ln -sv /usr/clang/bin/llvm-* /usr/gcc64/bin
 ln -sv /usr/clang/bin/lld /usr/gcc64/bin
