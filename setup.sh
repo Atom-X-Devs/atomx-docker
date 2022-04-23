5f3c5a2474d0d2c11f2d14ddda958106a0b7f47e
OGIndian <67594857+Divyanshu-Modi@users.noreply.github.com>
2022-04-23 16:01:51 +0530

    setup: Install default-jre and depmod
    • default-jre for zipsigner and depmod for modules
    
    Change-Id: If3498b08fc169bec4f05ecbd11af3d50ff9bcdc7

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 apt-get dist-upgrade -y -qq && apt-get upgrade -y -qq && apt-get update -y -qq
 ln -snf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime && echo Asia/Kolkata > /etc/timezone
-apt-get install --no-install-recommends -y -qq aria2 bc bison ca-certificates cpio curl file gcc git lib{c6,c,ssl,xml2}-dev make python3 python3-pip python2 unzip zip wget flex
+apt-get install --no-install-recommends -y -qq aria2 bc bison ca-certificates cpio curl file gcc git lib{c6,c,ssl,xml2}-dev make python3 python3-pip python2 unzip zip wget flex default-jre kmod
 pip3 install telegram-send
 apt-get autoremove -y && apt-get clean autoclean && rm -rf /var/lib/apt/lists/*
 curl -L https://github.com/AkihiroSuda/clone3-workaround/releases/download/v1.0.0/clone3-workaround.x86_64 -o noclone3 && chmod +x noclone3
