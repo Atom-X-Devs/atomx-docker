d8b9e713d28c3c95adb1eb1f7c05e5e5f2562bfb
Kunmun <kunmun@aospa.co>
2022-05-25 11:35:16 +0530

    docker: Switch over to ubuntu's default
    Change-Id: I9e585d36820ca1e67cd56abeb0b8d218342d4b18

--- setup.sh
+++ setup.sh
@@ -6,35 +6,3 @@ apt-get install --no-install-recommends -y -qq aria2 bc bison ca-certificates cp
 pip3 install telegram-send
 apt-get autoremove -y && apt-get clean autoclean && rm -rf /var/lib/apt/lists/*
 curl -L https://github.com/AkihiroSuda/clone3-workaround/releases/download/v1.0.0/clone3-workaround.x86_64 -o noclone3 && chmod +x noclone3
-
-t2h() {
-    echo "$1" | od -An -tx1 | tr -d '\n' | head -n1 | sed "s/ 0a$//g;s/ /\\\x/g"
-}
-
-rep() {
-    LANG=C grep -robUaPHn "$(t2h '$1')" -l | while read -r f; do
-        echo "${f}:${1}->${2}" && sed -i "s|$(t2h "$1")|$(t2h "$2")|g" "$f"
-    done
-}
-
-rem() {
-    cd /usr/"$1" && while read -r line; do rm -rfv "$line"; done < "/remove-$(echo "$1" | sed "s/32//g;s/64//g").txt" && cd /
-}
-
-get() {
-    curl -LSs  "https://codeload.github.com/$1/zip/$2" -o "$3".zip
-    unzip "$3".zip -d. && rm "$3".zip && mv -v "${1##*/}-$2" "/usr/${3}"
-    find "/usr/${3}" -exec chmod +x {} \; && rem "$3"
-}
-
-get mvaisakh/gcc-arm64-gnu main gcc64
-get mvaisakh/gcc-arm gcc-master gcc32
-curl -LSs https://gitlab.com/ElectroPerf/atom-x-clang/-/archive/atom-15/atom-x-clang-atom-15.zip -o "clang".zip
-unzip "clang".zip -d. && rm "clang".zip && mv -v "atom-x-clang-atom-15" "/usr/clang"
-find "/usr/clang" -exec chmod +x {} \; && rem "clang"
-
-ln -sv /usr/clang/bin/llvm-* /usr/gcc64/bin
-ln -sv /usr/clang/bin/lld /usr/gcc64/bin
-ln -sv /usr/clang/bin/ld.lld /usr/gcc64/bin
-
-chmod +x strip.sh && /strip.sh / && rm -rfv strip.sh ./*.txt
