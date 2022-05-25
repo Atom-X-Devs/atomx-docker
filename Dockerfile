c096efbbe5d6bb208f7f7571fafa957646549354
Akhil Narang <me@akhilnarang.dev>
2022-05-25 11:39:19 +0530

    setup: Initialise docker for aosp build
    Change-Id: I82268fccd5aac0fb74acd01ada626400970669c1

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM ubuntu:devel
 COPY ["setup.sh", "/"]
 RUN DEBIAN_FRONTEND=noninteractive chmod +x /setup.sh && /setup.sh && rm /setup.sh
+RUN apt-get update && apt-get install -y sudo
+RUN useradd -m -N -s /bin/bash -u 1000 -p 'atomx' atomx-devs && usermod -aG sudo atomx-devs
+USER atomx-devs
 SHELL ["/noclone3", "/bin/bash", "-c"]
--- setup.sh
+++ setup.sh
@@ -2,7 +2,69 @@
 
 apt-get dist-upgrade -y -qq && apt-get upgrade -y -qq && apt-get update -y -qq
 ln -snf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime && echo Asia/Kolkata > /etc/timezone
-apt-get install --no-install-recommends -y -qq aria2 bc bison ca-certificates cpio curl file gcc git lib{c6,c,ssl,xml2}-dev make python3 python3-pip python2 unzip zip wget flex default-jre kmod
+apt-get install --no-install-recommends -y -qq aria2 bc bison ca-certificates cpio curl file gcc git lib{c6,c,ssl,xml2}-dev make python3 python3-pip python2 unzip zip wget flex default-jre kmod tmate ssh python-is-python3 screen nano
 pip3 install telegram-send
 apt-get autoremove -y && apt-get clean autoclean && rm -rf /var/lib/apt/lists/*
 curl -L https://github.com/AkihiroSuda/clone3-workaround/releases/download/v1.0.0/clone3-workaround.x86_64 -o noclone3 && chmod +x noclone3
+
+# Setup an AOSP Build environment on Ubuntu and Linux Mint
+
+LATEST_MAKE_VERSION="4.3"
+UBUNTU_16_PACKAGES="libesd0-dev"
+UBUNTU_20_PACKAGES="libncurses5 curl python-is-python3"
+DEBIAN_10_PACKAGES="libncurses5"
+DEBIAN_11_PACKAGES="libncurses5"
+PACKAGES=""
+
+echo "Adding GitHub apt key and repository!"
+apt install software-properties-common -y
+apt-key adv --keyserver hkps://keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+apt-add-repository https://cli.github.com/packages
+
+apt update
+
+# Install lsb-core packages
+apt install lsb-core -y
+
+LSB_RELEASE="$(lsb_release -d | cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//')"
+
+if [[ ${LSB_RELEASE} =~ "Mint 18" || ${LSB_RELEASE} =~ "Ubuntu 16" ]]; then
+    PACKAGES="${UBUNTU_16_PACKAGES}"
+elif [[ ${LSB_RELEASE} =~ "Ubuntu 20" || ${LSB_RELEASE} =~ "Ubuntu 21" || ${LSB_RELEASE} =~ "Ubuntu 22" ]]; then
+    PACKAGES="${UBUNTU_20_PACKAGES}"
+elif [[ ${LSB_RELEASE} =~ "Debian GNU/Linux 10" ]]; then
+    PACKAGES="${DEBIAN_10_PACKAGES}"
+elif [[ ${LSB_RELEASE} =~ "Debian GNU/Linux 11" ]]; then
+    PACKAGES="${DEBIAN_11_PACKAGES}"
+fi
+
+DEBIAN_FRONTEND=noninteractive \
+    apt install \
+    adb autoconf automake axel bc bison build-essential \
+    ccache clang cmake expat fastboot flex g++ \
+    g++-multilib gawk gcc gcc-multilib git gnupg gperf \
+    htop imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 libc6-dev libcap-dev \
+    libexpat1-dev libgmp-dev '^liblz4-.*' '^liblzma.*' libmpc-dev libmpfr-dev libncurses5-dev \
+    libsdl1.2-dev libssl-dev libtool libxml2 libxml2-utils '^lzma.*' lzop \
+    maven ncftp ncurses-dev patch patchelf pkg-config pngcrush \
+    pngquant python2.7 python-all-dev re2c schedtool squashfs-tools subversion \
+    texinfo unzip w3m xsltproc zip zlib1g-dev lzip \
+    libxml-simple-perl libswitch-perl apt-utils gh \
+    ${PACKAGES} -y
+
+echo -e "Setting up udev rules for adb!"
+curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules -O -L https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules
+chmod 644 /etc/udev/rules.d/51-android.rules
+chown root /etc/udev/rules.d/51-android.rules
+
+if [[ "$(command -v make)" ]]; then
+    makeversion="$(make -v | head -1 | awk '{print $3}')"
+    if [[ ${makeversion} != "${LATEST_MAKE_VERSION}" ]]; then
+        echo "Installing make ${LATEST_MAKE_VERSION} instead of ${makeversion}"
+        bash "$(dirname "$0")"/make.sh "${LATEST_MAKE_VERSION}"
+    fi
+fi
+
+echo "Installing repo"
+curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
+chmod a+rx /usr/local/bin/repo
